39037ab2d5392cab28353cefa9815b552608a7dd
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2024-03-31 18:38:13 +0530

    Initial Files and Folder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Contact from './components/pages/contact/Contact';
+import Organization from './components/pages/organization/Organization';
+import Services from './components/pages/services/Services';
+import Home from './components/pages/home/Home';
+import Navigation from './components/shared/Navigation';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className='sidebar'>
+        <h1>React App</h1>
+        <Navigation />
+      </div>
+      <Routes className="content-page">
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/organization" element={<Organization />} />
+        <Route path="/services" element={<Services />} />
+      </Routes>
     </div>
   );
 }
